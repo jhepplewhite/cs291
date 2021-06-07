b7db7c73c842732a785117b4fa932117c68276b6
Jon Hepplewhite <jon_h1995@hotmail.co.uk>
2021-06-07 14:25:03 +0100

    Unit 5: Generating and rotating cylinders

--- unit5/unit5-ps_cylinder_exercise.js
+++ unit5/unit5-ps_cylinder_exercise.js
@@ -28,9 +28,9 @@ function createCylinderFromEnds( material, radiusTop, radiusBottom, top, bottom,
 	openEnded = (openEnded === undefined) ? false : openEnded;
 
 	// Dummy settings, replace with proper code:
-	var length = 100;
-	var cylAxis = new THREE.Vector3(100,100,-100);
-	var center = new THREE.Vector3(-100,100,100);
+	var length = top.distanceTo(bottom);
+	var cylAxis = new THREE.Vector3().subVectors( top, bottom );
+	var center = top.lerp(bottom, 0.5);
 	////////////////////
 
 	var cylGeom = new THREE.CylinderGeometry( radiusTop, radiusBottom, length, segmentsWidth, 1, openEnded );
